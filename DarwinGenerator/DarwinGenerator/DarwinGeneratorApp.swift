3eca8cc00304395c25688edf8d9cd8ab24b1b8b3
Lucas Farah <lucas.farah@me.com>
2023-02-21 15:17:46 -0300

    Initial project

---
+++ DarwinGenerator/DarwinGenerator/DarwinGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarwinGeneratorApp.swift
+//  DarwinGenerator
+//
+//  Created by Lucas Farah on 21/02/23.
+//
+
+import SwiftUI
+
+@main
+struct DarwinGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
